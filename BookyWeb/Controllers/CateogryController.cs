d621e69351d567bef2ca3213f90502d29fc67035
Youssef El-Nemaky <youssef.mmdoh99@gmail.com>
2025-08-23 15:48:13 +0300

    Add create action and create view

--- BookyWeb/Controllers/CateogryController.cs
+++ BookyWeb/Controllers/CateogryController.cs
@@ -1,4 +1,5 @@
-﻿using BookyWeb.Repositries;
+﻿using BookyWeb.Models;
+using BookyWeb.Repositries;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BookyWeb.Controllers
@@ -12,11 +13,31 @@ namespace BookyWeb.Controllers
             this.categoryRepo = categoryRepo;
         }
 
+        [HttpGet]
         public IActionResult Index()
         {
             var categoryList = categoryRepo.GetAll();
 
             return View("Index", categoryList);
         }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View("Create");
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category newCategory)
+        {
+            if (ModelState.IsValid)
+            {
+                categoryRepo.Add(newCategory);
+                categoryRepo.Save();
+                return RedirectToAction("Index");
+            }
+            return View("Create");
+        }
+
     }
 }
