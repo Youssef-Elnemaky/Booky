078b8cfbd3c7b642d66845413a404e576fa400ee
Youssef El-Nemaky <youssef.mmdoh99@gmail.com>
2025-08-23 14:42:53 +0300

    Add ICategoryRepository and CategoryRepositry

---
+++ BookyWeb/Repositries/CategoryRepository.cs
@@ -0,0 +1,49 @@
+﻿using BookyWeb.Models;
+using BookyWeb.Models.Data;
+
+namespace BookyWeb.Repositries
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly AppDbContext context;
+
+        public CategoryRepository(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+        public void Add(Category category)
+        {
+            context.Add(category);
+        }
+
+        public void Delete(int id)
+        {
+            Category? category = context.Categories.FirstOrDefault(c => c.Id == id);
+            if (category != null)
+            {
+                context.Categories.Remove(category);
+            }
+        }
+
+        public List<Category> GetAll()
+        {
+            return context.Categories.ToList();
+        }
+
+        public Category? GetById(int id)
+        {
+            return context.Categories.FirstOrDefault(c => c.Id == id);
+        }
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+
+        public void Update(Category category)
+        {
+            context.Update(category);
+        }
+    }
+}
---
+++ BookyWeb/Repositries/ICategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using BookyWeb.Models;
+
+namespace BookyWeb.Repositries
+{
+    public interface ICategoryRepository
+    {
+        public void Add(Category category);
+        public void Update(Category category);
+        public void Delete(int id);
+        public Category? GetById(int id);
+        public List<Category> GetAll();
+        public void Save();
+    }
+}
