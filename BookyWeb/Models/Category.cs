83c7f64b4d85c82d29e070aa9aa607e51a4bc941
Youssef El-Nemaky <youssef.mmdoh99@gmail.com>
2025-08-23 15:47:27 +0300

    Add validation attributes for category

--- BookyWeb/Models/Category.cs
+++ BookyWeb/Models/Category.cs
@@ -1,9 +1,19 @@
-﻿namespace BookyWeb.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookyWeb.Models
 {
     public class Category
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Category name is required")]
+        [MinLength(3, ErrorMessage ="Category name must be at least 3 characters")]
+        [MaxLength(100, ErrorMessage ="Category name cannot exceed 100 characters")]
+        [RegularExpression("^[a-zA-Z]*$", ErrorMessage = "Only letters are allowed.")]
         public string Name { get; set; }
+
+        [Display(Name = "Display Order")]
+        [Range(1,100, ErrorMessage = "Display Order must be in range of 1 - 100")]
         public int DisplayOrder { get; set; }
     }
 }
