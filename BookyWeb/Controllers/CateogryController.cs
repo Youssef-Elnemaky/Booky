3efe4cf93f84d6075e000c29c5178306fe841195
Youssef El-Nemaky <youssef.mmdoh99@gmail.com>
2025-08-23 15:04:36 +0300

    Add CategoryController with Index action to get all categories

---
+++ BookyWeb/Controllers/CateogryController.cs
@@ -0,0 +1,22 @@
+﻿using BookyWeb.Repositries;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryRepository categoryRepo;
+
+        public CategoryController(ICategoryRepository categoryRepo)
+        {
+            this.categoryRepo = categoryRepo;
+        }
+
+        public IActionResult Index()
+        {
+            var categoryList = categoryRepo.GetAll();
+
+            return View("Index", categoryList);
+        }
+    }
+}
