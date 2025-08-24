5a4172a90764bc8a0871499b74b5996e32930902
Youssef El-Nemaky <youssef.mmdoh99@gmail.com>
2025-08-24 04:10:05 +0300

    Add index action and simple view

---
+++ BookyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using Booky.DataAccess.Repositries.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookyWeb.Areas.Admin.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork unitOfWork;
+
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var products = unitOfWork.Product.GetAll();
+
+            return View("Index", products);
+        }
+
+
+    }
+}
