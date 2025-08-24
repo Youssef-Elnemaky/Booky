34d7e2ddebd253343c42a09fef2df0b57b5fd7e4
Youssef El-Nemaky <youssef.mmdoh99@gmail.com>
2025-08-24 04:03:28 +0300

    Add IProductReposiotry & ProductRepository

---
+++ Booky.DataAccess/Repositries/ProductRepository.cs
@@ -0,0 +1,26 @@
+﻿using Booky.DataAccess.Data;
+using Booky.DataAccess.Repositries.IRepository;
+using Booky.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booky.DataAccess.Repositries
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private readonly AppDbContext context;
+
+        public ProductRepository(AppDbContext context) : base(context) 
+        {
+            this.context = context;
+        }
+
+        public void Update(Product product)
+        {
+            context.Update(product);
+        }
+    }
+}
