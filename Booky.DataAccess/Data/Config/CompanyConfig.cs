4c4cacbfce37fccf603bb7c5b9a6a4bb505dfe19
Youssef El-Nemaky <youssef.mmdoh99@gmail.com>
2025-08-26 04:49:39 +0300

    Add seed data for company table

--- Booky.DataAccess/Data/Config/CompanyConfig.cs
+++ Booky.DataAccess/Data/Config/CompanyConfig.cs
@@ -21,6 +21,40 @@ namespace Booky.DataAccess.Data.Config
             builder.Property(c => c.PostalCode).HasColumnType("VARCHAR(20)").IsRequired(false);
             builder.Property(c => c.PhoneNumber).HasColumnType("VARCHAR(20)").IsRequired(false);
 
+            //seeding
+            builder.HasData(
+                [
+                    new Company
+                    {
+                        Id = 1,
+                        Name = "TechNova Solutions",
+                        StreetAddress = "123 Innovation Drive",
+                        City = "San Francisco",
+                        State = "CA",
+                        PostalCode = "94105",
+                        PhoneNumber = "+1-415-555-1234"
+                    },
+                    new Company
+                    {
+                        Id = 2,
+                        Name = "GreenLeaf Publishing",
+                        StreetAddress = "456 Maple Street",
+                        City = "Seattle",
+                        State = "WA",
+                        PostalCode = "98101",
+                        PhoneNumber = "+1-206-555-5678"
+                    },
+                    new Company
+                    {
+                        Id = 3,
+                        Name = "Global Bookstore Inc.",
+                        StreetAddress = "789 Oxford Road",
+                        City = "Boston",
+                        State = "MA",
+                        PostalCode = "02110",
+                        PhoneNumber = "+1-617-555-9012"
+                    }
+                ]);
 
         }
     }
---
+++ Booky.DataAccess/Migrations/20250826013119_SeedCompanyTable.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Booky.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedCompanyTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Companies",
+                columns: new[] { "Id", "City", "Name", "PhoneNumber", "PostalCode", "State", "StreetAddress" },
+                values: new object[,]
+                {
+                    { 1, "San Francisco", "TechNova Solutions", "+1-415-555-1234", "94105", "CA", "123 Innovation Drive" },
+                    { 2, "Seattle", "GreenLeaf Publishing", "+1-206-555-5678", "98101", "WA", "456 Maple Street" },
+                    { 3, "Boston", "Global Bookstore Inc.", "+1-617-555-9012", "02110", "MA", "789 Oxford Road" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Companies",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Companies",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Companies",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
