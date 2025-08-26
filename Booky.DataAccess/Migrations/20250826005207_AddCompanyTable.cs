aa5572ab1c6eecf865283eecb75f810b533193dd
Youssef El-Nemaky <youssef.mmdoh99@gmail.com>
2025-08-26 03:53:07 +0300

    Add migration to the DB to add Company table

---
+++ Booky.DataAccess/Migrations/20250826005207_AddCompanyTable.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Booky.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCompanyTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Companies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "VARCHAR(200)", nullable: false),
+                    StreetAddress = table.Column<string>(type: "VARCHAR(250)", nullable: true),
+                    City = table.Column<string>(type: "VARCHAR(100)", nullable: true),
+                    State = table.Column<string>(type: "VARCHAR(50)", nullable: true),
+                    PostalCode = table.Column<string>(type: "VARCHAR(20)", nullable: true),
+                    PhoneNumber = table.Column<string>(type: "VARCHAR(20)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Companies", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Companies");
+        }
+    }
+}
