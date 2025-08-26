aad22b5a41ab9bd4bfee49a75fd76c70ec7d9e38
Youssef El-Nemaky <youssef.mmdoh99@gmail.com>
2025-08-26 03:51:10 +0300

    Add ICompanyRepository & CompanyRepository

---
+++ Booky.DataAccess/Repositries/CompanyRepository.cs
@@ -0,0 +1,26 @@
+﻿using Booky.DataAccess.Data;
+using Booky.DataAccess.Repositries.IRepository;
+using Booky.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booky.DataAccess.Repositries
+{
+    public class CompanyRepository : Repository<Company>, ICompanyRepository
+    {
+        private readonly AppDbContext context;
+
+        public CompanyRepository(AppDbContext context) : base(context)
+        {
+            this.context = context;
+        }
+        public void Update(Company company)
+        {
+            // unless you want to do extra work
+            context.Update(company);
+        }
+    }
+}
